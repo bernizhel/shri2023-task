79df3a6095a8c607cc1610090f002d029b1c8290
bernizhel <detrop.1995@gmail.com>
2023-07-04 13:14:12 +0300

    build: adjust `vite.config.js` for prod

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
 	plugins: [react()],
+	base: "https://bernizhel.github.io/shri2023-task/",
+	build: {
+		outDir: "dist",
+	},
 });
