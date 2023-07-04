c40264991591e2f15a4067b39a751cb737d85bda
bernizhel <detrop.1995@gmail.com>
2023-07-04 14:47:47 +0300

    perf: add chunking strategy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-	plugins: [react()],
+	plugins: [splitVendorChunkPlugin(), react()],
 	base: "https://bernizhel.github.io/shri2023-task/",
 	build: {
 		outDir: "docs",
